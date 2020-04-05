6d45eb03a42388f755ba5708beaa1dea218c6cdb
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-04-05 22:43:37 +0800

    [logos] creation method replace constructor

--- ChainConstructors/MyWork/Loan.cs
+++ ChainConstructors/MyWork/Loan.cs
@@ -4,50 +4,44 @@ namespace ChainConstructors.MyWork
 {
     public class Loan
     {
-        private readonly CapitalStrategy _strategy;
+        private CapitalStrategy _strategy;
         private float _notional;
         private float _outstanding;
         private int _rating;
         private DateTime _expiry;
         private DateTime _maturity;
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry)
+        public Loan()
         {
-			this._strategy = new TermROC();
-			this._notional = notional;
-			this._outstanding = outstanding;
-			this._rating = rating;
-            this._expiry = expiry;
         }
 
-        public Loan(float notional, float outstanding, int rating, DateTime expiry, DateTime maturity) 
+        public static Loan CreateTermLoan(float notional, float outstanding, int rating, DateTime expiry)
         {
-            this._strategy = new RevolvingTermROC();
-            this._notional = notional;
-            this._outstanding = outstanding;
-            this._rating = rating;
-            this._expiry = expiry;
-            this._maturity = maturity;
+            var capitalStrategy = new TermROC();
+            return new Loan(capitalStrategy, notional, outstanding, rating, expiry, null);
         }
 
-        public Loan(CapitalStrategy strategy, float notional, float outstanding, 
-                    int rating, DateTime expiry, DateTime maturity)
+        public static Loan CreateTermLoan(float notional, float outstanding, int rating, DateTime expiry,
+            DateTime maturity)
+        {
+            var revolvingTermRoc = new RevolvingTermROC();
+            return new Loan(revolvingTermRoc, notional, outstanding, rating, expiry, maturity);
+        }
+
+        private Loan(CapitalStrategy strategy, float notional, float outstanding,
+            int rating, DateTime expiry, DateTime? maturity)
         {
             this._strategy = strategy;
             this._notional = notional;
             this._outstanding = outstanding;
             this._rating = rating;
             this._expiry = expiry;
-            this._maturity = maturity;
+            if (maturity != null) this._maturity = (DateTime) maturity;
         }
 
-		public CapitalStrategy CapitalStrategy
-		{
-			get
-			{
-				return _strategy;
-			}
-		}
-		
+        public CapitalStrategy CapitalStrategy
+        {
+            get { return _strategy; }
+        }
     }
 }
--- RefactoringToPatterns.Tests/ChainConstructors/MyWork/CapitalCalculationTests.cs
+++ RefactoringToPatterns.Tests/ChainConstructors/MyWork/CapitalCalculationTests.cs
@@ -1,29 +1,29 @@
-﻿using NUnit.Framework;
+﻿﻿using NUnit.Framework;
 using ChainConstructors.MyWork;
 using System;
 
 namespace RefactoringToPatterns.ChainConstructors.MyWork
 {
     [TestFixture]
-	public class CapitalCalculationTests
-	{
+    public class CapitalCalculationTests
+    {
         [SetUp]
-		public void Init()
-		{
-		}
+        public void Init()
+        {
+        }
 
         [Test]
         public void four_parameter_constructor_yields_loan_with_TermROC_strategy()
-		{
-            Loan loan = new Loan(1.0f, 2.0f, 4, new DateTime());
+        {
+            var loan = Loan.CreateTermLoan(1.0f, 2.0f, 4, new DateTime());
             Assert.IsInstanceOf(typeof(TermROC), loan.CapitalStrategy);
-		}
+        }
 
         [Test]
-        public void five_parameter_constructor_yields_revolving_TermROC_strategy() 
+        public void five_parameter_constructor_yields_revolving_TermROC_strategy()
         {
-            Loan loan = new Loan(1.0f, 2.0f, 4, new DateTime(), new DateTime());
+            var loan = Loan.CreateTermLoan(1.0f, 2.0f, 4, new DateTime(), new DateTime());
             Assert.IsInstanceOf(typeof(RevolvingTermROC), loan.CapitalStrategy);
         }
-	}
+    }
 }
